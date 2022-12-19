b3389e61150c0e17618b315538d8dc687b41e101
John Mota <88064321+John-Mota@users.noreply.github.com>
2022-12-19 13:19:39 -0300

    inicio

---
+++ Calculadora_type/app/type/index.ts
@@ -0,0 +1,8 @@
+class olaTypeScript{
+    constructor(public message: string) {
+
+    }
+}
+
+let ola = new olaTypeScript("Hello Word");
+console.log(ola.message);
---
+++ Calculadora_type/dist/index.js
@@ -0,0 +1,8 @@
+class olaTypeScript {
+    constructor(message) {
+        this.message = message;
+    }
+}
+let ola = new olaTypeScript("Hello Word");
+console.log(ola.message);
+//# sourceMappingURL=index.js.map
